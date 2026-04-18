df5dadddc285d33a31d1a7f3b7b738c0354822b0
Lakshit Verma <lakshit26v@gmail.com>
2026-04-18 10:58:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: 5173,
+    host: '0.0.0.0',  
+    port: 3005,
+    strictPort: true,
   },
-});
+})
